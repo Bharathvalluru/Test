3341ff0cc59d2e00a79bc561f264ff17801d58e6
valluru.bharath <bharath.valluru@Reltio.com>
2017-11-28 14:34:42 +0530

    Code Changes and using existing method to setting the property for chrome driver

--- HotelBookingTest.java
+++ HotelBookingTest.java
@@ -3,12 +3,13 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
 
 public class HotelBookingTest {
 
-    WebDriver driver = new ChromeDriver();
+    WebDriver driver ;
 
     @FindBy(linkText = "Hotels")
     private WebElement hotelLink;
@@ -24,30 +25,21 @@ public class HotelBookingTest {
 
     @Test
     public void shouldBeAbleToSearchForHotels() {
-        setDriverPath();
-
+        FlightBookingTest.setDriverPath();
+        driver = new ChromeDriver();
         driver.get("https://www.cleartrip.com/");
-        hotelLink.click();
+        HotelBookingTest page = PageFactory.initElements(driver,HotelBookingTest.class);
+        page.hotelLink.click();
 
-        localityTextBox.sendKeys("Indiranagar, Bangalore");
+        page.localityTextBox.sendKeys("Indiranagar, Bangalore");
 
-        new Select(travellerSelection).selectByVisibleText("1 room, 2 adults");
-        searchButton.click();
+        new Select(page.travellerSelection).selectByVisibleText("1 room, 2 adults");
+        page.searchButton.click();
 
         driver.quit();
 
     }
 
-    private void setDriverPath() {
-        if (PlatformUtil.isMac()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver");
-        }
-        if (PlatformUtil.isWindows()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-        }
-        if (PlatformUtil.isLinux()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver_linux");
-        }
-    }
+
 
 }
