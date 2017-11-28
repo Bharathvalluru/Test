302eb0566654ec071d99206f7eb10dcff554e795
valluru.bharath <bharath.valluru@Reltio.com>
2017-11-28 14:30:06 +0530

    Code Changes, Corrections of incorrect element locators and adding implict wait instead of Fluent wait

--- FlightBookingTest.java
+++ FlightBookingTest.java
@@ -9,63 +9,49 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class FlightBookingTest {
 
-    WebDriver driver = new ChromeDriver();
+    WebDriver driver ;
 
 
     @Test
     public void testThatResultsAppearForAOneWayJourney() {
         System.out.println("abcd");
         setDriverPath();
+        driver= new ChromeDriver();
         driver.get("https://www.cleartrip.com/");
-        waitFor(2000);
+        driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
         driver.findElement(By.id("OneWay")).click();
 
         driver.findElement(By.id("FromTag")).clear();
         driver.findElement(By.id("FromTag")).sendKeys("Bangalore");
 
-        //wait for the auto complete options to appear for the origin
 
-        waitFor(2000);
         List<WebElement> originOptions = driver.findElement(By.id("ui-id-1")).findElements(By.tagName("li"));
         originOptions.get(0).click();
 
-        driver.findElement(By.id("toTag")).clear();
-        driver.findElement(By.id("toTag")).sendKeys("Delhi");
+        driver.findElement(By.id("ToTag")).clear();
+        driver.findElement(By.id("ToTag")).sendKeys("Delhi");
 
-        //wait for the auto complete options to appear for the destination
 
-        waitFor(2000);
         //select the first item from the destination auto complete list
         List<WebElement> destinationOptions = driver.findElement(By.id("ui-id-2")).findElements(By.tagName("li"));
         destinationOptions.get(0).click();
 
-        driver.findElement(By.xpath("//*[@id='ui-datepicker-div']/div[1]/table/tbody/tr[3]/td[7]/a")).click();
+        driver.findElement(By.xpath("//*[@id='ui-datepicker-div']/div[2]/table/tbody/tr[3]/td[7]/a")).click();
 
         //all fields filled in. Now click on search
         driver.findElement(By.id("SearchBtn")).click();
 
-        waitFor(5000);
+
         //verify that result appears for the provided journey search
         Assert.assertTrue(isElementPresent(By.className("searchSummary")));
 
         //close the browser
         driver.quit();
-
     }
-
-
-    private void waitFor(int durationInMilliSeconds) {
-        try {
-            Thread.sleep(durationInMilliSeconds);
-        } catch (InterruptedException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-        }
-    }
-
-
     private boolean isElementPresent(By by) {
         try {
             driver.findElement(by);
@@ -75,14 +61,15 @@ public class FlightBookingTest {
         }
     }
 
-    private void setDriverPath() {
-        if (PlatformUtil.isMac()) {
+    public static void setDriverPath() {
+        if (System.getProperty("os.name").contains("Mac")) {
             System.setProperty("webdriver.chrome.driver", "chromedriver");
         }
-        if (PlatformUtil.isWindows()) {
+        if (System.getProperty("os.name").contains("Windows")) {
+            System.out.println();
             System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
         }
-        if (PlatformUtil.isLinux()) {
+        if (System.getProperty("os.name").contains("Linux")) {
             System.setProperty("webdriver.chrome.driver", "chromedriver_linux");
         }
     }
